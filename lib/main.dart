99a9aa08fc60bbf795c056029309e7a1936ebb28
Emircan-Bagdu52 <95845060+Emircan-Bagdu52@users.noreply.github.com>
2022-05-24 23:48:59 +0300

    Update main.dart
    Admob initialize eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:project_akademi/auth/auth_page.dart';
 import 'package:project_akademi/screens/timer_page.dart';
+import 'admob.dart';
 import 'auth/router_page.dart';
 import 'firebase_options.dart';
 import 'screens/method_desc_page.dart';
@@ -8,6 +10,7 @@ import 'screens/method_desc_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Admob.admobInitialize();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -21,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TimerPage(),
+      home: RouterPage(),
     );
   }
 }
