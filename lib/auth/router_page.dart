da5ccb14d14cff65b2c300937d25bab95c3e256d
Emircan-Bagdu52 <95845060+Emircan-Bagdu52@users.noreply.github.com>
2022-05-24 23:55:56 +0300

    Update router_page.dart

--- lib/auth/router_page.dart
+++ lib/auth/router_page.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_akademi/screens/home_view.dart';
 
+import '../screens/eisenhower_view.dart';
 import '../screens/home_page.dart';
+import '../screens/profile_view.dart';
 import 'auth_page.dart';
 
 // yönlendirme sayfası - giriş yapıldı mı yapılmadı mı??
 class RouterPage extends StatelessWidget {
+  static String routeName = 'router';
   const RouterPage({Key? key}) : super(key: key);
 
   @override
@@ -15,7 +19,7 @@ class RouterPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData){
-            return HomePage();
+            return HomeView();
           }else{
             return AuthPage();
           }
