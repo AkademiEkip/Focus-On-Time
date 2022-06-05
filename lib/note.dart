11e0b652e4279366de5d6fc52bb5efcb70c767a4
Emircan-Bagdu52 <95845060+Emircan-Bagdu52@users.noreply.github.com>
2022-06-05 16:02:00 +0300

    Update note.dart

--- lib/note.dart
+++ lib/note.dart
@@ -1,4 +1,11 @@
 class Note {
+
+  Note(String note, int noteCategory){
+    this.note = note;
+    this.noteCategory = noteCategory;
+  }
+
   String? note;
+  int? noteCategory;
   Function? function;
 }
